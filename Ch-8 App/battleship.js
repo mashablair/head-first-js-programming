9bf82383127c109f86b57aa277c3a477f6c007ef
Maria Blair <maria.klachko@gmail.com>
2016-09-20 13:02:21 -0700

    Chapter 8, added html and css to the Battleshipt game in index-game.html. Started the View object in battleship.js. Made minor revisions and comments in my original battleshipt game from Chapter 2.

---
+++ Ch-8 App/battleship.js
@@ -0,0 +1,24 @@
+// MVC
+// Model - keeps track of the ships, where they are, if they've been hit, and if they've been sunk
+// View - updates display w/ hits, missed and messages to user
+// Controller - glues them together, gets user's input and executes game's logic
+
+// object-oriented design
+
+var view = {
+    // this method takes a string and displays it in message window
+    displayMessage: function(msg) {
+        // some code
+    },
+
+    // this method displays 'miss' when user's input is wrong
+    displayMiss: function() {
+        // some code
+    },
+
+    // this method displays 'ship' when user's input is right
+    displayHit: function() {
+        //some code
+    }
+
+};
