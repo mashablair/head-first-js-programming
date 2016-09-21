77839d0c54e4e2474ca3e03e229439585b8607cd
Maria Blair <maria.klachko@gmail.com>
2016-09-21 16:03:11 -0700

    Battleship Game, added two methods to Model -- model.fire and model.isSunk -- in battleship.js. Chapter 8.

--- Ch-8 App/battleship.js
+++ Ch-8 App/battleship.js
@@ -30,7 +30,7 @@ var view = {
 
 };
 
-// testing:
+// testing View:
 view.displayMessage('Uh oh!');
 view.displayHit('00'); // A0
 view.displayMiss('34'); // D4
@@ -46,28 +46,72 @@ var model = {
     // ship locations and hits:
     ships: [{ locations: ['31', '41', '51'], hits: ['', '', ''] },
             { locations: ['14', '24', '34'], hits: ['', 'hit', ''] },
-            { locations: ['00', '01', '02'], hits: ['hit', '', ''] },]
+            { locations: ['00', '01', '02'], hits: ['hit', '', ''] },],
 
     // //method to fire on a ship and figure out if 'hit' or 'miss':
-    // fire: function() {
+    fire: function(guess) {
+        for (var i=0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            // we could write a loop to go through 'locations' but .indexOf() is easier:
+            var index = ship.locations.indexOf(guess); // it returns the index of the guess value
+            // and if the value is not in the array, it returns '-1'
+            if (index >= 0) { // if the value of user's guess was found in the array
+                // we have a HIT!
+                ship.hits[index] = 'hit'; // adds 'hit' to hits []
+                console.log('HIT!');
+                view.displayHit(guess); // updates View
+                view.displayMessage('You hit my ship!'); // updates View
 
-    // }
+                // our isSunk() method here:
+                if (this.isSunk(ship)) { // if isSunk() is true:
+                    view.displayMessage('You sank my battleship!'); // updates View
+                    this.shipsSunk++; // updates # of sunk ships above
+                }
+                return true;
+            } else {
+                console.log('MISSED!');
+                view.displayMiss(guess); // updates View
+                view.displayMessage('Sorry, you missed!'); // updates View
+                return false;
+            }
+        }
+    },
+
+    // this method will check if we have 3 hits and return true or false:
+    // this method will then be used in fire() method above.
+    isSunk: function(ship) {
+        for (var i=0; i<this.numShips; i++) {
+            if (ship.hits[i] !== 'hit') { // if this spot doesn't have 'hit' value,
+                return false;
+            }
+        }
+        return true; // otherwise, all spots = 'hit' and ship is sunk
+    } // now we can use this method in our fire()
 };
 
-var ship2 = model.ships[1];
-var locations = ship2.locations;
-console.log('Location is ' + locations[1]);
+// var ship2 = model.ships[1];
+// var locations = ship2.locations;
+// console.log('Location is ' + locations[1]);
 
-var ship3 = model.ships[2];
-var hits = ship3.hits;
-if (hits[0] === 'hit') {
-    console.log('Au! We have been hit at location one!');
-}
+// var ship3 = model.ships[2];
+// var hits = ship3.hits;
+// if (hits[0] === 'hit') {
+//     console.log('Au! We have been hit at location one!');
+// }
 
-var ship1 = model.ships[0];
-var hits = ship1.hits;
-hits[2] = 'hit';
-console.log(hits[2]);
+// var ship1 = model.ships[0];
+// var hits = ship1.hits;
+// hits[2] = 'hit';
+// console.log(hits[2]);
+
+
+// testing Model:
+model.fire('31');
+console.log(model.shipsSunk);
+model.fire('41');
+console.log(model.shipsSunk);
+model.fire('51');
+console.log(model.shipsSunk);
 
 
 
