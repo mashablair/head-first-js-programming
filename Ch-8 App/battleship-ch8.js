029c9d1d547f0ac9e2cd7a25336763ad71fa4339
Maria Blair <maria.klachko@gmail.com>
2016-09-21 17:13:19 -0700

    Added more testing and comment to Chapter 8 Battleship Game.

--- Ch-8 App/battleship-ch8.js
+++ Ch-8 App/battleship-ch8.js
@@ -113,6 +113,21 @@ console.log(model.shipsSunk);
 model.fire('51');
 console.log(model.shipsSunk);
 
+// some more testing
+// model.fire("53"); // miss
+
+// model.fire("31"); // hit
+// model.fire("41"); // hit
+// model.fire("51"); // hit
+
+// model.fire("34"); // hit
+// model.fire("24"); // hit
+// model.fire("14"); // hit
+
+// model.fire("00"); // hit
+// model.fire("01"); // hit
+// model.fire("02"); // hit
+
 
 
 
