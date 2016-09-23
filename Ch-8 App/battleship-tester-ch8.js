55f3ec196b17f5817bcfc9ac6131051f008e967c
Maria Blair <maria.klachko@gmail.com>
2016-09-23 11:21:51 -0700

    Put all testing for the Battleship game Ch8 in battleship-tester-ch8.js

--- Ch-8 App/battleship-tester-ch8.js
+++ Ch-8 App/battleship-tester-ch8.js
@@ -1,6 +1,27 @@
-// some more testing
-// if ships' locations are hardcoded:
-model.fire("53"); // miss
+// To test things (like, View or Model), uncomment portions of code below:
+
+    // ***ships locations and hits HARDCODED for testing purposes:
+    // ships: [{ locations: ['31', '41', '51'], hits: ['', '', ''] },
+    //         { locations: ['14', '24', '34'], hits: ['', 'hit', ''] },
+    //         { locations: ['00', '01', '02'], hits: ['hit', '', ''] },],
+
+
+// ***testing VIEW:
+// view.displayMessage('Woa, look at this grid!');
+// view.displayMiss('00'); // 'A0'
+// view.displayHit('34'); // 'D4'
+
+
+// ***testing MODEL:
+// model.fire('31'); // should be 'hit'
+// console.log(model.shipsSunk);
+// model.fire('41'); // should be 'hit'
+// console.log(model.shipsSunk);
+// model.fire('51'); // 3rd hit
+// console.log(model.shipsSunk); // should be 1 now
+
+// ***testing IF locations are hardcoded:
+// model.fire("53"); // miss
 
 // model.fire("31"); // hit
 // model.fire("41"); // hit
@@ -14,16 +35,11 @@ model.fire("53"); // miss
 // model.fire("01"); // hit
 // model.fire("02"); // hit
 
-// console.log('Is there a problem?');
 
-controller.processGuess('D1');
-controller.processGuess('E1');
-controller.processGuess('F1');
-
-controller.processGuess('D4');
-controller.processGuess('C4');
-controller.processGuess('B4');
-
-controller.processGuess('A0');
-controller.processGuess('A1');
-controller.processGuess('A2');
+// ***testing CONTROLLER parseGuess():
+// console.log(parseGuess('A0')); // 00
+// console.log(parseGuess('kf')); // null
+// console.log(parseGuess('59')); // null
+// console.log(parseGuess('B8')); // null
+// console.log(parseGuess('F9')); // null
+// console.log(parseGuess('D6')); // 36
